e4d4b60d7532eb500b71f577acd7a0360163811b
oka <pn11@users.noreply.github.com>
2021-07-12 17:46:55 +0900

    remove timeout, add stop button

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# webrtc_recording_test
-JSを使ってブラウザ上で録音をしてみるテスト
+# Interaxion Recorder
+
+[mnao305/webrtc_recording_test: JSを使ってブラウザ上で録音をしてみるテスト](https://github.com/mnao305/webrtc_recording_test)
+
+より fork させて頂きました。
--- main.js
+++ main.js
@@ -1,24 +1,27 @@
 import Recorder from "./lib/recorder.js"
 
 // ボタンとか取ってきてるだけ
-const btn = document.getElementById("recordingBtn")
+const recordBtn = document.getElementById("recordBtn")
+const stopBtn = document.getElementById("stopBtn")
 const player = document.getElementById("player")
 const downloadLink = document.getElementById("downloadLink")
 const status = document.getElementById("recordingStatus")
 const recorder = new Recorder()
 
-// ボタンがクリックされたら
-btn.addEventListener("click", () => {
+// 録音ボタンがクリックされたら開始
+recordBtn.addEventListener("click", () => {
   status.style = "display: inline;"
   downloadLink.style = "display: none;"
   player.src = ""
   recorder.start()
-  setTimeout(() => {
-    // 10秒後に停止する
-    const url = recorder.stop()
-    player.src = url
-    downloadLink.href = url
-    status.style = "display: none;"
-    downloadLink.style = "display: inline;"
-  }, 10 * 1000)
+})
+
+// 停止ボタンがクリックされたら停止
+stopBtn.addEventListener("click", () => {
+  player.src = ""
+  const url = recorder.stop()
+  player.src = url
+  downloadLink.href = url
+  status.style = "display: none;"
+  downloadLink.style = "display: inline;"
 })
